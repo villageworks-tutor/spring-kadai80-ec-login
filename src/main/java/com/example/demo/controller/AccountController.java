8660311306485e03f74cb0b5f2579b1f00e644d4
villageworks <villageworks@gmail.com>
2023-12-07 16:32:56 +0900

    ver.1.21: 会員登録時の入力チェック

--- src/main/java/com/example/demo/controller/AccountController.java
+++ src/main/java/com/example/demo/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -59,6 +62,50 @@ public class AccountController {
 			@RequestParam(name = "email", defaultValue = "") String email,
 			@RequestParam(name = "password", defaultValue = "") String password,
 			Model model) {
+		// リクエストパラメータの入力チェック
+		List<String> errors = new ArrayList<String>();
+		// 名前の入力チェック
+		if (name.isEmpty()) {
+			// 空文字列の場合：必須入力エラー
+			errors.add("名前は必須です");
+		}
+		// 住所の入力チェック
+		if (address.isEmpty()) {
+			// 空文字列の場合：必須入力エラー
+			errors.add("住所は必須です");
+		}
+		// 電話番号の入力チェック
+		if (tel.isEmpty()) {
+			// 空文字列の場合：必須入力エラー
+			errors.add("電話番号は必須です");
+		}
+		// 電子メールの入力チェック
+		if (email.isEmpty()) {
+			// 空文字列の場合：必須入力エラー
+			errors.add("e-mailは必須です");
+		} else if (customerRepository.findByEmail(email) != null) {
+			// 登録済のメールアドレスの場合：重複エラー
+			errors.add("登録済のメールアドレスです");
+		}
+		// パスワードの入力チェック
+		if  (password.isEmpty()) {
+			// 空文字列の場合：必須入力エラー
+			errors.add("パスワードは必須です");
+		}
+		
+		// エラーがある場合
+		if (errors.size() > 0) {
+			// エラーメッセージをスコープに登録
+			model.addAttribute("errors", errors);
+			// 入力値をスコープに登録
+			model.addAttribute("name", name);
+			model.addAttribute("address", address);
+			model.addAttribute("tel", tel);
+			model.addAttribute("email", email);
+			// 自画面遷移
+			return "accountForm";
+		}
+		
 		// リクエストパラメータをもとに顧客インスタンスを生成
 		Customer customer = new Customer(name, address, tel, email, password);
 		// 顧客インスタンスの永続化
